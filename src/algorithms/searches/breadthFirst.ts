525b09fee07ff593e67f6eb6b394a4c90c4b9a72
SamuelRCrider <sam.crider@aol.com>
2024-06-25 15:05:18 -0400

    visualized breadth first search

---
+++ src/algorithms/searches/breadthFirst.ts
@@ -0,0 +1,40 @@
+import {
+  BSResult,
+  ReturnOfCreateNode,
+} from "@/components/searchAlgos/BreadthFirst";
+
+export const breadthFirstSearch = (
+  node: ReturnOfCreateNode,
+  numToFind: number
+): BSResult[] => {
+  if (!node) {
+    return [];
+  }
+
+  const queue = [node];
+  const result: BSResult[] = [];
+
+  while (queue.length) {
+    // remove last node from stack
+    const current = queue.shift();
+    // check if equal to numToFind
+    if (current?.value === numToFind) {
+      result.push({ value: current.value, found: true });
+      return result;
+    }
+    // else push value and check children
+    result.push({ value: current?.value, found: false });
+
+    // if node has left child, push child to stack
+    if (current?.left) {
+      queue.push(current.left);
+    }
+
+    // if node has right child, push child to stack
+    if (current?.right) {
+      queue.push(current.right);
+    }
+  }
+
+  return result;
+};
