1a29ae63812a96557225edecc3bf70a71842affb
SamuelRCrider <sam.crider@aol.com>
2024-06-26 13:24:35 -0400

    partial

---
+++ src/algorithms/advanced/djikstra.ts
@@ -0,0 +1,14 @@
+import { Node } from "@/components/advancedAlgos/Djikstra";
+
+export const wrappedDjikstra = (graph: Node[]) => {
+  // copy graph
+  let copyGraph: Node[] = graph;
+  // TODO: change this type
+  let steps: any[] = [];
+
+  const djikstra = (graph: Node[]) => {};
+
+  djikstra(copyGraph);
+
+  return steps;
+};
--- src/components/advancedAlgos/Djikstra.tsx
+++ src/components/advancedAlgos/Djikstra.tsx
@@ -1,19 +1,54 @@
-import React from "react";
+import { wrappedDjikstra } from "@/algorithms/advanced/djikstra";
+import React, { useEffect, useState } from "react";
 import { GraphCanvas } from "reagraph";
 
-const distances = [
+// type
+export type Node = {
+  node: string;
+  data: {
+    d: number;
+    nodes: string[];
+  }[];
+};
+
+/* setting up graph */
+
+// all connections with their distances
+const distances: {
+  d: number;
+  nodes: string[];
+}[] = [
   { d: 1, nodes: ["A", "B"] },
-  { d: 2, nodes: ["A", "G"] },
+  { d: 2, nodes: ["A", "E"] },
+  { d: 1, nodes: ["B", "C"] },
+  { d: 2, nodes: ["E", "D"] },
+  { d: 2, nodes: ["D", "C"] },
 ];
 
-const getWeights = (letter: string, d: typeof distances) => {
-  // get all distances where distance.nodes includes h
-};
-const getNode = (letter: string) => {
-  return { data: getWeights(letter, distances), node: letter };
+// function that searches through distances and returns all that
+// include the current node (letter)
+const getDistances = (
+  letter: string,
+  d: typeof distances
+): typeof distances => {
+  // get all distances where distance.nodes includes letter
+  let allDistances: typeof distances = [];
+  d.forEach((currD) => {
+    if (currD.nodes.includes(letter)) {
+      allDistances.push(currD);
+    }
+  });
+  return allDistances;
 };
 
-const graph = [
+// function that gets all the distances of the current node (letter)
+// returns both the node identifier (letter) and all its connections in an object
+const getNode = (letter: string): Node => {
+  return { node: letter, data: getDistances(letter, distances) };
+};
+
+// graph of nodes
+const graph: Node[] = [
   getNode("A"),
   getNode("B"),
   getNode("C"),
@@ -21,7 +56,25 @@ const graph = [
   getNode("E"),
 ];
 
+// default steps init
+// TODO: change any type
+const defaultSteps: any[] = [];
+
 const Djikstra = () => {
+  const [steps, setSteps] = useState(defaultSteps);
+  const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+    const steps = wrappedDjikstra(graph);
+    setSteps(steps);
+  }, []);
+
+  useEffect(() => {
+    if (counter < steps.length - 1) {
+      setTimeout(() => setCounter(counter + 1), 1000);
+    }
+  }, [counter]);
+
   return (
     <div className="flex flex-col justify-evenly h-full w-full items-center">
       <div className="flex flex-col items-center gap-6">
@@ -94,7 +147,7 @@ const Djikstra = () => {
         <div className="flex flex-row items-center gap-6">
           <div className="flex flex-col items-center gap-1">
             <div className="border-b-2 border-black">Find Shortest Path</div>
-            <div className="text-xl font-bold">A {"->"} E</div>
+            <div className="text-xl font-bold">A {"->"} C</div>
           </div>
           <button
             className="btn"
